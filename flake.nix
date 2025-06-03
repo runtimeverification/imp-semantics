efc150c50e83e73d6167ad3e2919a92831756e9e
Julian Kuners <julian.kuners@gmail.com>
2025-06-03 13:48:09 +0200

    add nix derivation of kimp

---
+++ flake.nix
@@ -0,0 +1,86 @@
+{
+  description = "IMP Semantics";
+  inputs = {
+    rv-nix-tools.url = "github:runtimeverification/rv-nix-tools/854d4f05ea78547d46e807b414faad64cea10ae4";
+    nixpkgs.follows = "rv-nix-tools/nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    k-framework.url = "github:runtimeverification/k/v7.1.259";
+    k-framework.inputs.nixpkgs.follows = "nixpkgs";
+    uv2nix.url = "github:pyproject-nix/uv2nix/680e2f8e637bc79b84268949d2f2b2f5e5f1d81c";
+    uv2nix.inputs.nixpkgs.follows = "nixpkgs";
+    pyproject-build-systems.url = "github:pyproject-nix/build-system-pkgs/7dba6dbc73120e15b558754c26024f6c93015dd7";
+    pyproject-build-systems = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.pyproject-nix.follows = "uv2nix/pyproject-nix";
+    };
+    pyproject-nix.follows = "uv2nix/pyproject-nix";
+  };
+  outputs = { self, rv-nix-tools, nixpkgs, flake-utils, pyproject-nix, pyproject-build-systems, uv2nix, k-framework }:
+  let
+    pythonVer = "310";
+  in flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ] (system:
+    let
+      # due to the nixpkgs that we use in this flake being outdated, uv is also heavily outdated
+      # we can instead use the binary release of uv provided by uv2nix for now
+      uvOverlay = final: prev: {
+        uv = uv2nix.packages.${final.system}.uv-bin;
+      };
+      # create custom overlay for k, because the overlay in k-framework currently also includes a lot of other stuff instead of only k
+      kOverlay = final: prev: {
+        k = k-framework.packages.${final.system}.k;
+      };
+      kimpOverlay = final: prev:
+      let
+        kimp-pyk = final.callPackage ./nix/kimp-pyk {
+          inherit pyproject-nix pyproject-build-systems uv2nix;
+          python = final."python${pythonVer}";
+        };
+        kimp = final.callPackage ./nix/kimp {
+          inherit kimp-pyk;
+          rev = self.rev or null;
+        };
+      in {
+        inherit kimp;
+      };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          uvOverlay
+          kOverlay
+          kimpOverlay
+        ];
+      };
+      python = pkgs."python${pythonVer}";
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "uv develop shell";
+        buildInputs = [
+          python
+          pkgs.uv
+        ];
+        env = {
+          # prevent uv from managing Python downloads and force use of specific 
+          UV_PYTHON_DOWNLOADS = "never";
+          UV_PYTHON = python.interpreter;
+        };
+        shellHook = ''
+          unset PYTHONPATH
+        '';
+      };
+      packages = rec {
+        inherit (pkgs) kimp uv;
+        default = kimp;
+      };
+    }) // {
+      overlays.default = final: prev: {
+        inherit (self.packages.${final.system}) kimp;
+      };
+    };
+}
--- nix/kimp/default.nix
+++ nix/kimp/default.nix
@@ -1,55 +1,43 @@
 {
   lib,
+  stdenv,
+  makeWrapper,
   callPackage,
-  nix-gitignore,
 
-  pyproject-nix,
-  pyproject-build-systems,
-  uv2nix,
+  k,
+  which,
 
-  python
-}:
-let
-  pyproject-util = callPackage pyproject-nix.build.util {};
-  pyproject-packages = callPackage pyproject-nix.build.packages {
-    inherit python;
-  };
+  kimp-pyk,
+  rev ? null
+} @ args:
 
-  # load a uv workspace from a workspace root
-  workspace = uv2nix.lib.workspace.loadWorkspace {
-    workspaceRoot = lib.cleanSource (nix-gitignore.gitignoreSourcePure [
-        ../../.gitignore
-        ".github/"
-        "result*"
-        # do not include submodule directories that might be initilized empty or non-existent due to nix/git
-        # otherwise cachix build might not match the version that is requested by `kup`
-        # TODO: for new projects, add your submodule directories that are not required for nix builds here!
-        # e.g., `"/docs/external-computation"` with `external-computation` being a git submodule directory
-        # "/docs/external-computation"
-      ] ../..
-    );
-  };
+stdenv.mkDerivation {
+  pname = "kimp";
+  version = if (rev != null) then rev else "dirty";
+  buildInputs = [
+    kimp-pyk
+    k
+  ];
+  nativeBuildInputs = [ makeWrapper ];
 
-  # create overlay
-  lockFileOverlay = workspace.mkPyprojectOverlay {
-    # prefer "wheel" over "sdist" due to maintance overhead
-    # there is no bundled set of overlays for "sdist" in uv2nix, in contrast to poetry2nix
-    sourcePreference = "wheel";
-  };
+  src = callPackage ../kimp-source { };
 
-  buildSystemsOverlay = import ./build-systems-overlay.nix;
+  dontUseCmakeConfigure = true;
 
-  # construct package set
-  pythonSet = pyproject-packages.overrideScope (lib.composeManyExtensions [
-    # make build tools available by default as these are not necessarily specified in python lock files
-    pyproject-build-systems.overlays.default
-    # include all packages from the python lock file
-    lockFileOverlay
-    # add build system overrides to certain python packages
-    buildSystemsOverlay
-  ]);
-in pyproject-util.mkApplication {
-  # default dependancy group enables no optional dependencies and no dependency-groups
-  venv = pythonSet.mkVirtualEnv "kimp-env" workspace.deps.default;
-  package = pythonSet.kimp;
+  enableParallelBuilding = true;
+
+  buildPhase = ''
+    XDG_CACHE_HOME=$(pwd) ${
+      lib.optionalString
+      (stdenv.isAarch64 && stdenv.isDarwin)
+      "APPLE_SILICON=true"
+    } kimp-kdist -v build 'imp-semantics.*'
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r ./kdist-*/* $out/
+    mkdir -p $out/bin
+    makeWrapper ${kimp-pyk}/bin/kimp $out/bin/kimp --prefix PATH : ${lib.makeBinPath [ which k ]} --set KDIST_DIR $out
+  '';
 }
